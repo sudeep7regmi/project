22438a9d2cd9ac271e90e8d59ee346cee11a9b5e
sudeep7regmi <sudeepofficial01@gmail.com>
2025-07-31 20:28:27 +0545

    Add files via upload

---
+++ authmiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'your_secret_key_here';
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+function authorizeRole(role) {
+  return (req, res, next) => {
+    if (req.user.role !== role) return res.sendStatus(403);
+    next();
+  };
+}
+
+module.exports = { authenticateToken, authorizeRole };
+
